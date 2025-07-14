967713d8dbc883c542b8afa8b6e69e17baa43a7c
Akshay K.a <106895549+akshay-k-a-dev@users.noreply.github.com>
2025-07-14 01:48:00 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import { Play, ArrowDown, MousePointer } from 'lucide-react';
 import hero1 from '../assets/Hero1.JPG';
 import hero2 from '../assets/Hero2.JPG';
-import hero3 from '../assets/Hero3.JPG';
+import hero3 from '../assets/Hero3.jpg';
 
 const Hero: React.FC = () => {
   const heroRef = useRef<HTMLElement>(null);
